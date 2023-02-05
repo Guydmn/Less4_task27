00b24e1a5e151b438839c8c729856438d8ba1ece
Roman Guydmn <romanguydmn@gmail.com>
2023-02-05 11:05:51 +0400

    Задача 27 Лекция 5

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int SumNumAmmount (int S)
+{
+    int count = 0;
+    int N = S;
+    while (S > 0)
+        {
+            N = S % 10;
+            count = N + count;
+            S = S / 10;
+        }    
+    return count;
+}
+
+Console.Write("Введите число: ");
+int S = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Сумма цифр в числе равна = {SumNumAmmount(S)}");
